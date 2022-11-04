8983be3f03bc26dfff461652475fff1bd1e9ac8a
lenoryv <luisortiz141999@gmail.com>
2022-11-04 10:36:40 -0500

    refactor: environment variables

--- microservice-a/src/app.module.ts
+++ microservice-a/src/app.module.ts
@@ -3,15 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.g8aip.mongodb.net/?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-      },
-    ),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.URL_MONGODB, {
+      useNewUrlParser: true,
+    }),
     UserModule,
   ],
   controllers: [AppController],
