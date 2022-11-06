365878d21a0391c4f1d5c6bd3c9ed43c28d9ef47
lenoryv <luisortiz141999@gmail.com>
2022-11-05 21:44:16 -0500

    add: RabbitMQ transporter

--- microservice-b/src/app.module.ts
+++ microservice-b/src/app.module.ts
@@ -5,9 +5,24 @@ import { ConfigModule } from '@nestjs/config';
 //Mongoose
 import { MongooseModule } from '@nestjs/mongoose';
 import { ReportSchema } from './schemas/report.schema';
+//Transport
+import { Transport, ClientsModule } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'reports_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
