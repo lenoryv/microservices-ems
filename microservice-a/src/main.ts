da8c21d44dc20ec94c7d7dcb830ff4b478386c0a
lenoryv <luisortiz141999@gmail.com>
2022-11-05 21:43:37 -0500

    add: RabbitMQ transporter

--- microservice-a/src/main.ts
+++ microservice-a/src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'reports_queue',
+      queueOptions: { durable: false },
+      prefetchCount: 1,
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
+  logger.log(`🚀 Service running on port ${configService.get('PORT')}`);
 }
 bootstrap();
