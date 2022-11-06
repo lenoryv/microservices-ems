e836a5169a1c2835af65b8da93a676008669f3ee
lenoryv <luisortiz141999@gmail.com>
2022-11-06 18:34:16 -0500

    refactor: main

--- microservice-b/src/main.ts
+++ microservice-b/src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3002);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+  logger.log(`🚀 Service running on port ${configService.get('PORT')}`);
 }
 bootstrap();
