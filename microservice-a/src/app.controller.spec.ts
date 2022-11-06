86b6c81824b783e4beba27232cf846232f39ca1f
lenoryv <luisortiz141999@gmail.com>
2022-11-06 18:48:38 -0500

    refactor: simple test app controller

--- microservice-a/src/app.controller.spec.ts
+++ microservice-a/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "All Employees!"', () => {
+      expect(appController.getEmployees('/')).toBe('All Employees!');
     });
   });
 });
